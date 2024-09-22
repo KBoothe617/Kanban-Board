4ac4ccc41d8a496803009de1f1102ea9464d5393
Your Name <kboothe617@gmail.com>
2024-09-22 15:28:32 -0600

    verify token exists and add user data to request object in auth.ts

--- Develop/server/src/middleware/auth.ts
+++ Develop/server/src/middleware/auth.ts
@@ -5,6 +5,26 @@ interface JwtPayload {
   username: string;
 }
 
-export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
+export const authenticateToken = (req: Request, res: Response, next: NextFunction): void => {
   // TODO: verify the token exists and add the user data to the request object
+  const authHeader = req.headers['authorization'];
+
+  if (authHeader) {
+    const token = authHeader.split(' ')[1];
+    const secretKey: string | undefined = process.env.JWT_SERCRET_KEY;
+    if (!secretKey) {
+      res.sendStatus(500); // Internal Server Error if secret key is not defined
+      return;
+    }
+    jwt.verify(token, secretKey, (err, user) => {
+      if (err) {
+        res.sendStatus(403);
+        return;
+      }
+      req.user = user as JwtPayload; // Add user data to the request object
+      next();
+    });
+  } else {
+    res.sendStatus(401);
+  }
 };
