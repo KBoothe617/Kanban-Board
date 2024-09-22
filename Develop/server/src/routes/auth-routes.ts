8c294576f688e2dee57b714d9ad4031d9665063a
Your Name <kboothe617@gmail.com>
2024-09-22 15:42:51 -0600

    user login validation and token return in auth-routes.ts

--- Develop/server/src/routes/auth-routes.ts
+++ Develop/server/src/routes/auth-routes.ts
@@ -5,7 +5,29 @@ import bcrypt from 'bcrypt';
 
 export const login = async (req: Request, res: Response) => {
   // TODO: If the user exists and the password is correct, return a JWT token
+  const { username, password } = req.body;
+
+  // search database for user
+  const user = await User.findOne({
+    where: { username },
+  });
+  if (!user) {
+    return res.sendStatus(401).json({ message: 'Failed to authenticate' });
+  }
+
+  // compare password
+  const passwordMatch = await bcrypt.compare(password, user.password);
+  if (!passwordMatch) {
+    return res.sendStatus(401).json({ message: 'Failed to authenticate' });
+  }
+
+  // get secret key
+  const secretKey = process.env.JWT_SECRET_KEY || '';
+  const token = jwt.sign({ username }, secretKey, { expiresIn: '1h' }); // create token that lasts 1 hour
+  res.json({ token });
+
 };
+
 
 const router = Router();
 
