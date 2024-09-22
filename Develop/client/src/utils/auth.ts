4cb3194faa27c1169c60b8e8a4c98e3c9a01c0e3
Your Name <kboothe617@gmail.com>
2024-09-22 16:29:37 -0600

    return a value indicating if the user is logged in and return a value indicating if the token is expired in auth.ts client side

--- Develop/client/src/utils/auth.ts
+++ Develop/client/src/utils/auth.ts
@@ -9,10 +9,20 @@ class AuthService {
 
   loggedIn() {
     // TODO: return a value that indicates if the user is logged in
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token);
   }
   
   isTokenExpired(token: string) {
     // TODO: return a value that indicates if the token is expired
+    try {
+      const decoded = jwtDecode<JwtPayload>(token);
+      if (decoded?.exp && decoded?.exp < Date.now() / 1000) {
+        return true;
+      }
+    } catch (err) {
+      return false;
+    }
   }
 
   getToken(): string {
