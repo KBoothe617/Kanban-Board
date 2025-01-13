661d717fa8d969bdda976bc499ea3cb80081d8d7
Your Name <kboothe617@gmail.com>
2025-01-12 23:11:48 -0700

    fixes

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -5,24 +5,24 @@ const login = async (userInfo: UserLogin) => {
   try {
     const response = await fetch('/auth/login', {
       method: 'POST',
-      headers : {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(userInfo),
-  });
+      headers: {
+        'Content-Type': 'application/json'
+      }, 
+      body: JSON.stringify(userInfo)
+    });
 
-  const data = await response.json();
+    const data = await response.json();
 
-  if (!response.ok) {
-    throw new Error('User info not retrived');
+    if(!response.ok) {
+      throw new Error('User information not retrieved, check network tab!');
+    }
+
+    return data;
+  } catch(err) {
+    console.log('Error from user login: ', err);
+    return Promise.reject('Could not fetch user info');
   }
-
-  return data;
-  } catch (err) {
-    console.log(err);
-    return Promise.reject(err);
-  }
-};
+}
 
 
 
