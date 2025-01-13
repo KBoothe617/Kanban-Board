f3e4e878c3f353becf71c2259a2f065c35bc0419
Your Name <kboothe617@gmail.com>
2025-01-12 22:58:59 -0700

    fix in auth.ts

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -11,20 +11,18 @@ export const authenticateToken = (req: Request, res: Response, next: NextFunctio
 
   if (authHeader) {
     const token = authHeader.split(' ')[1];
-    const secretKey: string | undefined = process.env.JWT_SERCRET_KEY;
-    if (!secretKey) {
-      res.sendStatus(500); // Internal Server Error if secret key is not defined
-      return;
-    }
+
+    const secretKey = process.env.JWT_SECRET_KEY || '';
+
     jwt.verify(token, secretKey, (err, user) => {
       if (err) {
-        res.sendStatus(403);
-        return;
+        return res.sendStatus(403); // Forbidden
       }
-      req.user = user as JwtPayload; // Add user data to the request object
-      next();
+
+      req.user = user as JwtPayload;
+      return next();
     });
   } else {
-    res.sendStatus(401);
+    res.sendStatus(401); // Unauthorized
   }
 };
